3e5decfe5b9c79329d040c667537eedb5da8233d
FarukKalil <faruk1.kalil@gmail.com>
2023-03-20 12:22:13 -0300

    Exercicio 7 finalizado

--- Exercicio7/src/exercicio7/Exercicio7.java
+++ Exercicio7/src/exercicio7/Exercicio7.java
@@ -1,9 +1,20 @@
 package exercicio7;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio7 {
 
     public static void main(String[] args) {
+        int qtafuncionarios, i;
+        double media = 0;
+        double salario, mediaSalarial = 0;
+        qtafuncionarios = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de funcionários da empresa: "));
+        for ( i = 1 ; i <= qtafuncionarios ; i++){
+            salario = Double.parseDouble(JOptionPane.showInputDialog("Digite o salário do " + i + "° funcionário:" ));
+            media = media + salario;
+        }
+        mediaSalarial = media/qtafuncionarios;
         
+        System.out.println("A média salarial da empresa EVOLUA é: "+mediaSalarial);
     }
-    
 }
